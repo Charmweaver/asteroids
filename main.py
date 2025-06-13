72aa01d4cbec15d7d2bc5a4bc5d63662ac18d004
Melanie <melanie@nomico.net>
2025-06-13 07:22:20 -0700

    pygame setup

---
+++ main.py
@@ -0,0 +1,11 @@
+# this allows us to use code from
+# the open-source pygame library
+# throughout this file
+import pygame
+
+print ("Starting Asteroids!")
+
+def main():
+    if __name__ == "__main__":
+        main()
+
